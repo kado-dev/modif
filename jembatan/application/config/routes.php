b9a9fb0970d383315578319b422fb9aeff4664df
Dodi Syaripudin <dodisyaripudindev@gmail.com>
2025-12-05 02:08:31 +0700

    Delete jembatan directory

--- jembatan/application/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$route['default_controller'] = 'home';
-$route['404_override'] = 'error';
-$route['translate_uri_dashes'] = FALSE;
-
