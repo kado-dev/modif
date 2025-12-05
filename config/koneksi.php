cded0b96083ab42b73fcacf0acbb861d010fc745
Dodi Syaripudin <dodisyaripudindev@gmail.com>
2025-12-05 22:43:52 +0700

    delete info

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,14 +1,1 @@
-<?php
-	$servername = "103.245.39.167";
-	$username = "dblinggarpkmuser";
-	$password = "Tomi481216!";
-	$nama_db = "dblinggarpkm";
-	$kode_puskesmas = "P3204110202";
-	$koneksi =mysqli_connect($servername,$username,$password,$nama_db);
 
-	//$servername = "103.125.88.159";
-	//$username = "dblinggar";
-	//$password = "Arkana2016@#";
-	//$nama_db = "simpusilp";
-	//$koneksi =mysqli_connect($servername,$username,$password,$nama_db);
-?>
